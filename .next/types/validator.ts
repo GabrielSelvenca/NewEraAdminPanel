ac637f8d5cd6eadd98f076e1fae6856e8912f178
GabrielSelvenca <gabriel.selvenca@icloud.com>
2025-12-25 17:57:11 -0300

    chore: Deletar página antiga de customers não utilizada

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -63,15 +63,6 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
-// Validate ../../src/app/dashboard/customers/page.tsx
-{
-  type __IsExpected<Specific extends AppPageConfig<"/dashboard/customers">> = Specific
-  const handler = {} as typeof import("../../src/app/dashboard/customers/page.js")
-  type __Check = __IsExpected<typeof handler>
-  // @ts-ignore
-  type __Unused = __Check
-}
-
 // Validate ../../src/app/dashboard/deliveries/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/dashboard/deliveries">> = Specific
